5a2a7f0c3b0afe2ab94560ebf02d2ab76412239d
igo903 <igo903@hotmail.com>
2014-05-21 17:18:46 +0800

    Angular init2

--- Mo/js/app.js
+++ Mo/js/app.js
@@ -1,4 +1,21 @@
-var app = angular.module("app", ['ngRoute','ngAnimate','ngTouch']);
+var app = angular.module("wholApp", ['ngRoute','ngAnimate','ngTouch']);
+
+//url mapping
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/Dashboard/Default',
+            {
+                controller: 'defaultController',
+                templateUrl: 'partials/default.html',
+                title: 'logo'
+            })
+});
+
+
+app.service('dataService', function($http, $route) {
+
+});
+
 
 app.controller('homelistController', function($scope, dataService, $filter) {
 
