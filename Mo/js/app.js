5e1fb068652b95b99074297fb07982569631cd06
igo903 <igo903@hotmail.com>
2014-05-22 22:37:04 +0800

    angular home list test success

--- Mo/js/app.js
+++ Mo/js/app.js
@@ -2,33 +2,37 @@ var app = angular.module("wholApp", ['ngRoute','ngAnimate','ngTouch']);
 
 //url mapping
 app.config(function($routeProvider) {
-    $routeProvider
-        .when('/Dashboard/Default',
-            {
-                controller: 'defaultController',
-                templateUrl: 'partials/default.html',
-                title: 'logo'
-            })
-        .otherwise(
-        	{
-        		redirectTo: '/Dashboard/Default'
-        	}
-        )
+    // $routeProvider
+    //     .when('/Dashboard/Default',
+    //         {
+    //             controller: 'homelistController',
+    //             templateUrl: 'partials/homelist.html',
+    //             title: 'logo'
+    //         })
+    //     .otherwise(
+    //     	{   controller: 'homelistController',
+    //             templateUrl: 'partials/homelist.html',
+    //     		redirectTo: '/Dashboard/Default'
+    //     	}
+    //     )
 });
 
 
 app.service('dataService', function($http, $route) {
-	this.getToRead = function() {
-        return $http.get('data/homelist.json', {
-                'cache': true
-        });
-    };
+
+    this.getToRead = function() {
+                return $http.get('data/homelist.json', {
+                        'cache': true
+                });
+        };
 
 });
 
 
 app.controller('homelistController', function($scope, dataService) {
-	dataService.getToread().success(function(){
+	dataService.getToRead().success(function(data){
 		$scope.toreads = data;
 	});
 });
+
+
